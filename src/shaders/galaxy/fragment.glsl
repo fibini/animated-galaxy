f50e5874474c608a64d6c528db69d420b18199ab
Fabien <fabienswift@gmail.com>
2023-01-01 12:43:11 -0500

    Draw  particle patterns

--- src/shaders/galaxy/fragment.glsl
+++ src/shaders/galaxy/fragment.glsl
@@ -1,4 +1,23 @@
 void main()
 {
-    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    // // Disc
+    // float strength = distance(gl_PointCoord, vec2(0.5));
+    // strength = step(0.5, strength);
+    // strength = 1.0 - strength;
+
+    // gl_FragColor = vec4(vec3(strength), 1.0);
+
+    // // Diffuse point
+    // float strength = distance(gl_PointCoord, vec2(0.5));
+    // strength *= 2.0 ;
+    // strength = 1.0 - strength;
+
+    // gl_FragColor = vec4(vec3(strength), 1.0);
+
+    // Light point
+    float strength = distance(gl_PointCoord, vec2(0.5));
+    strength = 1.0 - strength;
+    strength = pow(strength, 10.0);
+
+    gl_FragColor = vec4(vec3(strength), 1.0);
 }
